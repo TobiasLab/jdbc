cf08df2f506e835af2efc973aa9888e443e6f12c
TobiasLab <tobias.labidi@gmail.com>
2019-10-20 22:03:53 +0200

    Lots of code that I do no understand :)

--- src/test/java/no/kristiania/jdbc/WebshopTest.java
+++ src/test/java/no/kristiania/jdbc/WebshopTest.java
@@ -1,18 +1,22 @@
 package no.kristiania.jdbc;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.Random;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class WebshopTest {
     @Test
-    void shouldRetriveStoredProduct() {
-        ProductDao dao = new ProductDao();
-        String productName = pickOne(new String[] {"Apples", "Bananas", "Coconuts", "Dates"});
-        dao.insertProduct(productName);
-        assertThat(dao.listAll()).contains(productName);
+    void shouldRetrieveStoredProduct() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test");
+
+        dataSource.getConnection().createStatement().executeUpdate(
+                "create table products (name varchar(100))"
+        );
     }
 
     private String pickOne(String[] strings) {
